ef9f403396fb8b20463580b1124a066b799edf73
kirankumar <kiran14585@gmail.com>
2022-12-23 14:37:57 +0530

    completed teams model

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
-
+from .models import Team
 
 # Create your views here.
 def home(request):
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
 
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/home.html',data)
 
 
 def about(request):
-
-    return render(request, 'pages/about.html')
+    teams = Team.objects.all()
+    data = {
+        'teams': teams,
+    }
+    return render(request, 'pages/about.html', data)
 
 def services(request):
 
@@ -18,5 +25,3 @@ def services(request):
 def contact(request):
 
     return render(request, 'pages/contact.html')
-
-  
